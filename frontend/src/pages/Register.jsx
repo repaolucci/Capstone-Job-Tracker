852c79c1057bac1bb7ccf82927b6dfa8d64dcf4e
kentair <itskent@proton.me>
2023-02-15 06:56:58 -0800

    fin.registration alert, not accepting new users

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-
+import { useHistory } from "react-router-dom";
 
 export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
+    let history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email);
         console.log(pass);
+        console.log(name);
+        
     }
 
     return (
@@ -25,7 +28,10 @@ export const Register = (props) => {
                 <label htmlFor="password">password</label>
                 <input value = {pass} onChange={(e) => setPass(e.target.value)}id="password" type="password" placeholder="**********" name="password"/>
                 
-                <button>Log In</button>
+                <button onClick={() => {
+                    alert("sorry not accepting new users at the moment");
+                    props.onFormSwitch('login');}
+                    }>Log In</button>
             </form>
             <button className='link-btn' onClick={() => props.onFormSwitch('login')}>Got an Account? Login here</button>
         </div>
