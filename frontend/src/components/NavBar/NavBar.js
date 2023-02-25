6aa7ce3b90c862e6d11bdd2db256b8adaed717c9
Rebecca Paolucci <repaolucci@gmail.com>
2023-02-25 14:21:20 -0600

    removed extraneous comments

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -17,45 +17,6 @@ const NavBar = () => {
     </ul>
   </div>
 </nav>
-
-
-
-    
-//     <nav className="navbar navbar-expand-lg py-4 navbar-dark bg-dark">
-//   <a className="navbar-brand" href="/view-page">
-//        Job Tracker
-//   </a>
-//   <button
-//     className="navbar-toggler"
-//     type="button"
-//     data-toggle="collapse"
-//     data-target="#navbarNavDropdown"
-//     aria-controls="navbarNavDropdown"
-//     aria-expanded="false"
-//     aria-label="Toggle navigation"
-//   >
-//     <span className="navbar-toggler-icon"></span>
-//   </button>
-//   <div className="collapse navbar-collapse" id="navbarNavDropdown">
-//     <ul className="navbar-nav">
-//       <li className="nav-item">
-//         <a className="nav-link" href="/view-page">
-//           Jobs
-//         </a>
-//       </li>
-//       <li className="nav-item">
-//         <a className="nav-link" href="/contacts-page">
-//           Contacts
-//         </a>
-//       </li>
-//       <li className="nav-item">
-//         <a className="nav-link" href="/skills-page">
-//           Skills
-//         </a>
-//       </li>
-//       </ul>
-//   </div>
-// </nav>
   );
 };
 
