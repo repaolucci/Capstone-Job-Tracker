05d9d8a2378a0072d4324bfc3d0283b83db090d6
Alexandra Rhodes <atperry30@gmail.com>
2023-02-27 11:42:37 -0800

    Created AddJob Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import HomePage from './pages/Homepage';
-import AddJobPage from './pages/AddJobPage';
-import EditJobPage from './pages/EditJobPage';
-import SkillsPage from './pages/SkillsPage';
-import { useState} from 'react';
-import Log_in from './pages/LoginPage';
-import { NavBar } from './components/NavBar';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
+import SkillsPage from "./pages/SkillsPage";
+import { useState } from "react";
+import Log_in from "./pages/LoginPage";
+import { NavBar } from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import AddJob from "./components/Modal/AddJob";
 
 function App() {
   const [jobToEdit, setJobToEdit] = useState();
@@ -17,43 +17,38 @@ function App() {
   return (
     <div className="App">
       <Router>
-        
         <div className="App-header">
           <Route path="/view-page">
-          <div className="navbar-custom">
-          <NavBar />
-          </div>
+            <div className="navbar-custom">
+              <NavBar />
+            </div>
             <HomePage setJobToEdit={setJobToEdit} />
           </Route>
           <Route path="/add-job">
-          <div className="navbar-custom">
-          <NavBar />
-          </div>
+            <div className="navbar-custom">
+              <NavBar />
+            </div>
             <AddJobPage />
           </Route>
           <Route path="/edit-job">
-          <div className="navbar-custom">
-          <NavBar />
-          </div>
+            <div className="navbar-custom">
+              <NavBar />
+            </div>
             <EditJobPage jobToEdit={jobToEdit} />
           </Route>
           <Route path="/skills-page">
-          <div className="navbar-custom">
-          <NavBar />
-          </div>
-          <SkillsPage />
+            <div className="navbar-custom">
+              <NavBar />
+            </div>
+            <SkillsPage />
           </Route>
           <Route path="/" exact>
             <Log_in />
           </Route>
-          </div>
+        </div>
       </Router>
     </div>
   );
-
-
-
-
 
   // return (
   //   <div className="App">
@@ -77,7 +72,6 @@ function App() {
   //         <Route path="/" exact>
   //           <Log_in />
   //         </Route>
-
 
   //         </div>
   //     </Router>
